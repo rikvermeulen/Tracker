5e3e4bdee031d4dd1b32e1b56d6ca5ea8fa66ba4
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2021-07-01 14:48:46 +0200

    Initial Commit

---
+++ Trackers/TrackersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackersApp.swift
+//  Trackers
+//
+//  Created by Rik Vermeulen on 01/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct TrackersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
